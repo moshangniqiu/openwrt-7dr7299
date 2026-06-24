b2895c4ea66596f49cca4d72607c1fcb62f5fae9
bob0021210 <106322801+moshangniqiu@users.noreply.github.com>
2026-06-24 22:52:30 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -5,7 +5,7 @@ echo "=== diy-script: 开始自定义编译配置 ==="
 
 # 修改默认IP
 echo "[diy] 修改默认IP为 192.168.123.1"
-sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 sed -i -E 's|^root:[^:]*:|root::|' package/base-files/files/etc/shadow
 
 # 移除要替换的包（来自官方 feeds）
@@ -29,12 +29,36 @@ clone_if_missing https://github.com/sbwml/luci-app-mosdns              "v5"     
 clone_if_missing https://github.com/QiuSimons/luci-app-daed            ""       package/dae
 clone_if_missing https://github.com/sbwml/v2ray-geodata               ""        package/v2ray-geodata
 
+set -e -o pipefail
+
+WORKSPACE_ROOT="${GITHUB_WORKSPACE:-$(pwd)}"
+GOLANG126_SRC_DIR="$WORKSPACE_ROOT/scripts/6.6/golang1.26"
+GOLANG126_FEED_DIR="feeds/packages/lang/golang1.26"
+rm -rf "$GOLANG126_FEED_DIR"
+mkdir -p "$GOLANG126_FEED_DIR"
+cp -rf "$GOLANG126_SRC_DIR/." "$GOLANG126_FEED_DIR/"
+./scripts/feeds update -f packages
+./scripts/feeds install golang1.26
+
+# passwall daed use golang1.26/host
+find package/dae -name "Makefile" -type f -exec sed -i \
+  -e 's|\<golang/golang-package.mk\>|golang1.26/golang-package.mk|g' \
+  -e 's|\<golang/host\>|golang1.26/host|g' {} +
+
+# 同步仓库内维护的 patches 目录到 OpenWrt 源码树
+if [ -d "$GITHUB_WORKSPACE/patches/6.6" ]; then
+  echo "[diy] 同步自定义 patches/6.6 目录到源码树"
+  cp -rf "$GITHUB_WORKSPACE/patches/6.6/." ./
+else
+  echo "[diy] patches/6.6 目录不存在，跳过"
+fi
+
 
 
 # 修改版本为编译日期
 DATE_VERSION="$(date +%Y.%m.%d)"
 VERSION_FILE="include/version.mk"
 echo "[diy] 修改版本为编译日期: $DATE_VERSION"
-sed -i "s/^VERSION_NUMBER:=.*/VERSION_NUMBER:=-$DATE_VERSION by WoChen5770/" "$VERSION_FILE"
+sed -i "s/^VERSION_NUMBER:=.*/VERSION_NUMBER:=-$DATE_VERSION moshangniqiu/" "$VERSION_FILE"
 
 echo "=== diy-script: 完成 ==="
