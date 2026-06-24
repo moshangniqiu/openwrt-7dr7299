d98fb8e61df12e8ccdb575fcc3058c61caf575d9
bob0021210 <106322801+moshangniqiu@users.noreply.github.com>
2026-06-24 22:50:36 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -25,15 +25,10 @@ clone_if_missing() {
   fi
 }
 
-clone_if_missing https://github.com/sbwml/luci-app-mosdns              ""     package/luci-app-mosdns
-clone_if_missing https://github.com/ximiTech/luci-app-msd_lite         ""     package/luci-app-msd_lite
-clone_if_missing https://github.com/ximiTech/msd_lite                  ""     package/msd_lite
-clone_if_missing https://github.com/pymumu/luci-app-smartdns           ""     package/luci-app-smartdns
-clone_if_missing https://github.com/pymumu/openwrt-smartdns            ""     package/smartdns
-clone_if_missing https://github.com/QiuSimons/luci-app-daed            ""     package/dae
-clone_if_missing https://github.com/Openwrt-Passwall/openwrt-passwall-packages "" package/passwall-packages
-clone_if_missing https://github.com/Openwrt-Passwall/openwrt-passwall  ""     package/passwall-luci
-clone_if_missing https://github.com/EasyTier/luci-app-easytier.git     ""     package/luci-app-easytier
+clone_if_missing https://github.com/sbwml/luci-app-mosdns              "v5"     package/luci-app-mosdns
+clone_if_missing https://github.com/QiuSimons/luci-app-daed            ""       package/dae
+clone_if_missing https://github.com/sbwml/v2ray-geodata               ""        package/v2ray-geodata
+
 
 
 # 修改版本为编译日期
